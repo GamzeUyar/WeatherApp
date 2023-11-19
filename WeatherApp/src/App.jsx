3034996afb099f1a12f8ed6e1db8f96eec869d2c
Gamze <gmzefe026gmailcom>
2023-11-20 02:01:37 +0300

    data çekildi, css kodları oluşturuldu

--- WeatherApp/src/App.jsx
+++ WeatherApp/src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weather, setWeather] = useState(null);
+  const [location, setLocation] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://api.weatherapi.com/v1/forecast.json?key=${
+            import.meta.env.VITE_WEATHER_API
+          }&q=${location}&days=5&aqi=yes&alerts=yes`
+        );
+        setWeather(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (location) {
+      fetchData();
+    }
+  }, [location]);
+
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app-container">
+        <h1 className="app-title">Hava Durumu Uygulaması</h1>
+
+        <div className="input-container">
+          <input
+            className="location-input"
+            type="text"
+            placeholder="Şehir Giriniz"
+            value={location}
+            onChange={handleLocationChange}
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {weather && (
+        <div className="weather-container">
+          {weather.forecast.forecastday.map((day) => (
+            <div className="day-container" key={day.date}>
+              <h2 className="date"> {day.date} </h2>
+              <img
+                className="weather-icon"
+                src={day.day.condition.icon}
+                alt={day.day.condition.text}
+              />
+              <p className="temperature"> {day.day.avgtemp_c} C </p>
+              <p className="temperature-text"> {day.day.condition.text} </p>
+            </div>
+          ))}
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
